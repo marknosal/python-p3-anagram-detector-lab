0a7f4b78f698ceb8f8852d0765c244a3487af0b2
Mark Nosal <mark.c.nosal@gmail.com>
2023-06-04 03:18:22 -0500

    finished lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,6 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, new_anagram):
+        self.new_anagram = new_anagram
+
+    def match(self, possible_anagrams):
+        return [word for word in possible_anagrams if sorted(word) == sorted(self.new_anagram)]
